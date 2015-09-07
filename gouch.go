f5b7d57d729e1378950743b542f00f82f57eba93
satojkovic <satojkovic@gmail.com>
2015-09-08 08:07:29 +0900

    Accept multiple files

--- gouch.go
+++ gouch.go
@@ -13,13 +13,16 @@ var (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: gouch file\n")
+		fmt.Fprintf(os.Stderr, "Usage: gouch file ...\n")
 		return
 	}
-	newFileName := os.Args[1]
-	newFile, err = os.Create(newFileName)
-	if err != nil {
-		log.Fatal(err)
+
+	for _, arg := range os.Args[1:] {
+		newFileName := arg
+		newFile, err = os.Create(newFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		newFile.Close()
 	}
-	newFile.Close()
 }
